79fe83d311d47dbcaef1076b411ee0b3fa0ec898
Dawn nguyen <dawnnguyen2021@gmail.com>
2023-11-11 21:30:12 -0500

    added event listeners

--- assets/script.js
+++ assets/script.js
@@ -10,35 +10,97 @@ var quizScreenFour = document.querySelector('.quiz-four');
 function showStart() {
     startScreen.style.display = null;
     quizScreen.style.display = "none";
+    quizScreenTwo.style.display = "none";
+    quizScreenThree.style.display = "none";
+    quizScreenFour.style.display = "none";
     endScreen.style.display = "none";
 }
 
 function showQuiz() {
     startScreen.style.display = "none";
     quizScreen.style.display = null;
+    quizScreenTwo.style.display = "none";
+    quizScreenThree.style.display = "none";
+    quizScreenFour.style.display = "none";
+    endScreen.style.display = "none";
+}
+
+function showQuizTwo() {
+    startScreen.style.display = "none";
+    quizScreen.style.display = "none";
+    quizScreenTwo.style.display = null;
+    quizScreenThree.style.display = "none";
+    quizScreenFour.style.display = "none";
+    endScreen.style.display = "none";
+}
+
+function showQuizThree() {
+    startScreen.style.display = "none";
+    quizScreen.style.display = "none";
+    quizScreenTwo.style.display = "none";
+    quizScreenThree.style.display = null;
+    quizScreenFour.style.display = "none";
+    endScreen.style.display = "none";
+}
+
+function showQuizFour() {
+    startScreen.style.display = "none";
+    quizScreen.style.display = "none";
+    quizScreenTwo.style.display = "none";
+    quizScreenThree.style.display = "none";
+    quizScreenFour.style.display = null;
     endScreen.style.display = "none";
 }
 
 function showEnd() {
     startScreen.style.display = "none";
     quizScreen.style.display = "none";
+    quizScreenTwo.style.display = "none";
+    quizScreenThree.style.display = "none";
+    quizScreenFour.style.display = "none";
     endScreen.style.display = null;
 }
-
-startButton.addEventListener('click', function(event) {
+// added event listeners
+startButton.addEventListener('click', function (event) {
     showQuiz();
 });
 
-quizScreen.addEventListener('click', function(event){
-if (event.target.matches('button'))
-console.log("hit button")
-}); 
+quizScreen.addEventListener('click', function (event) {
+    showQuizTwo();
+});
+
+quizScreenTwo.addEventListener('click', function (event) {
+    showQuizThree();
+});
+
+quizScreenThree.addEventListener('click', function (event) {
+    showQuizFour();
+});
+
+quizScreenFour.addEventListener('click', function (event) {
+    showEnd();
+});
+quizScreen.addEventListener('click', function (event) {
+    if (event.target.matches('button'))
+});
+
+quizScreenTwo.addEventListener('click', function (event) {
+    if (event.target.matches('button'))
+    });
+
+quizScreenThree.addEventListener('click', function (event) {
+    if (event.target.matches('button'))
+        });
+
+quizScreenFour.addEventListener('click', function (event) {
+    if (event.target.matches('button'))
+            });
 
 
 
 
-function init () {
-showStart();
+function init() {
+    showStart();
 }
 
 init();
